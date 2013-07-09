165fbd9356e375d77cd175d13103696c3fa489e4
tranquilinho <jesuscal@gmail.com>
2013-07-09 22:27:24 +0000

    Get text from index.html

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var index_contents=new Buffer(1024);
+    fs.readFileSync("index.html", function (err, data) {
+	if (err) throw err;
+	index_contents.write(data);
+    });
+    response.send(index_contents.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+
+    console.log("Listening on " + port);
 });
