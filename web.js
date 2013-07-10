81716ac95b15f47ff9c168425221e7a369231a03
tranquilinho <jesuscal@gmail.com>
2013-07-10 08:36:13 +0000

    encoding fix

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var index_contents=new Buffer(1024);
-    fs.readFileSync("index.html", function (err, data) {
+    fs.readFileSync("index.html", "ascii", function (err, data) {
 	if (err) throw err;
 	index_contents.write(data);
     });
